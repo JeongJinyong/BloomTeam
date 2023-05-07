b32d7887b74a359d7ebb4544ec3ea6a1b1c77b05
JJY <skwntnftkgk@gmail.com>
2023-05-07 21:26:57 +0900

    버전업데이트

--- app/src/main/java/com/bloom/emotional/postcard/presentation/SplashActivity.kt
+++ app/src/main/java/com/bloom/emotional/postcard/presentation/SplashActivity.kt
@@ -2,9 +2,11 @@ package com.bloom.emotional.postcard.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.bloom.emotional.postcard.R
+import com.bumptech.glide.Glide
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -13,6 +15,8 @@ class SplashActivity:AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val imageView = findViewById<ImageView>(R.id.splash)
+        Glide.with(this).load(R.drawable.splash).into(imageView)
         lifecycleScope.launch {
             delay(1000)
             startActivity(Intent(this@SplashActivity,MainActivity::class.java))
